40d6258728c4ebeecc11f80dd07c2fe0b76dc555
Niamask <niamaskay@gmail.com>
2023-09-15 14:29:32 -0700

    9. More classes!

--- console.py
+++ console.py
@@ -11,11 +11,18 @@ from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """HBNB class"""
     prompt = '(hbnb) '
-    classes = {'BaseModel': BaseModel, 'User': User}
+    classes = {'BaseModel': BaseModel, 'User': User, 'City': City,
+               'Place': Place, 'Amenity': Amenity, 'Review': Review,
+               'State': State}
 
 
     def do_EOF(self, line):
