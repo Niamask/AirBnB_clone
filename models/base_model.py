c4772b7dde84174cb2a90c975d9d4df44ab40ca7
Niamask <niamaskay@gmail.com>
2023-09-15 10:21:40 -0700

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,20 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    date = value[0:10] + " " + value[11:]
+                    self.created_at = datetime.strptime(
+                        date, '%Y-%m-%d %H:%M:%S.%f')
+                elif key == "updated_at":
+                    date = value[0:10] + " " + value[11:]
+                    self.updated_at = datetime.strptime(
+                        date, "%Y-%m-%d %H:%M:%S.%f")
+                elif key != "__class__":
+                    setattr(self, key, value)
+        models.storage.new(self)
+
     def __str__(self):
         """Return string representation for an object"""
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
